a653e71a1cf3f690e246e32301f8006492c2a22a
Mark Poko <markpoko2@gmail.com>
2020-12-23 07:01:56 -0500

    add wallpaper command to set MacOS Wallpaper

---
+++ cmd/wallpaper.go
@@ -0,0 +1,78 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/pokom/iterm-muzei/providers"
+	"github.com/spf13/cobra"
+)
+
+// wallpaperCmd represents the wallpaper command
+var wallpaperCmd = &cobra.Command{
+	Use:   "wallpaper",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		pictureDir := os.Getenv("PICTURE_DIR")
+		if len(pictureDir) == 0 {
+			log.Fatal("PICTURE_DIR Needs to be set")
+		}
+		muzeiClient := providers.NewMuzeiClient()
+		featured, err := muzeiClient.GetFeatured()
+		if err != nil {
+			log.Fatal(err)
+		}
+		imageFileName := providers.BuildFileName(featured.ImageURI)
+		fileName := filepath.Join(pictureDir, imageFileName)
+		file := providers.CreateFile(fileName)
+		err = providers.DownloadImage(file, featured.ImageURI)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Wrote out image to: %s\n", fileName)
+		command := exec.Command("bash", "set-desktop-img.sh", fileName)
+		command.Dir = "bin"
+		_, err = command.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("wallpaper called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(wallpaperCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// wallpaperCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// wallpaperCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
